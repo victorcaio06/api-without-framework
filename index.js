ae91d8856049ad59f08fe9cb0c02812b80b37bc2
Victor Caio <kaioaraujo426@gmail.com>
2023-02-15 15:54:43 -0300

    FEAT: routes created

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const http = require('http');
+const http = require("http");
 
 const port = 4444;
 
-const server = http.createServer((request, response) => {});
+const server = http.createServer((request, response) => {
+  if (request.url === "/firstRoute") {
+    const result = {
+      message: "First response",
+    };
 
-server.listen(port, () => console.log('Server is running!!'));
+    response.statusCode = 200;
+    response.setHeader("Content-type", "application/json");
+    return response.end(JSON.stringify(result));
+  } else if (request.url === "/secondRoute") {
+    const result = {
+      message: "Second response",
+    };
+
+    response.statusCode = 200;
+    response.setHeader("Content-type", "application/json");
+    return response.end(JSON.stringify(result));
+  } 
+
+  response.statusCode = 200;
+  response.setHeader("Content-type", "application/json");
+  response.write("Homepage");
+  response.end();
+});
+
+server.listen(port, () => console.log("Server is running!!"));

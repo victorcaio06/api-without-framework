29f172ccfc6d6f5bceca69ef36f34586025f71a1
Victor Caio <kaioaraujo426@gmail.com>
2023-02-20 12:23:18 -0300

    FEAT: added return response in controller

--- src/modules/user/useCases/listUser/listUsercontroller.js
+++ src/modules/user/useCases/listUser/listUsercontroller.js
@@ -3,8 +3,10 @@ const ListUserUseCase = require("./listUserUseCase");
 class ListUserController {
   listUserUseCase = new ListUserUseCase();
 
-  async handle() {
-    return await this.listUserUseCase.execute(); 
+  async handle(request, response) {
+    const users = await this.listUserUseCase.execute();
+
+    return response.end(JSON.stringify(users));
   }
 }
 
